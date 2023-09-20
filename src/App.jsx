9f31ab7a4d87fb4151187f6aebaff4ed30a64fad
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-20 07:34:38 +0100

    add dotenv and firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import dotenv from "dotenv"
+dotenv.config();
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId:process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId:process.env.measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); 
 
 function App() {
 
